f666d051f352f54cb94aa53a8830398a055329c8
Yevhenii-Bezeha <64944748+Yevhenii-Bezeha@users.noreply.github.com>
2021-07-25 08:45:43 +0000

    Deploying to gh-pages from @ Yevhenii-Bezeha/bc-project-team-2@8b325014564416ad91d550686eb314283204e867 🚀

--- src.107cdb78.js
+++ src.107cdb78.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/bc-project-team-2/src.d5e69836.js.map
+//# sourceMappingURL=/bc-project-team-2/src.107cdb78.js.map
