043d4ed7d335abf6a76abe9f74571a9a8354d7a9
Dmitry Dovgopol <dovgopol.ds@gmail.com>
2021-07-29 17:40:57 +0300

    add fixes for mob menu

--- src/js/menu.js
+++ src/js/menu.js
@@ -2,6 +2,8 @@
   const menuBtnRef = document.querySelector('[data-menu-button]');
   const mobileMenuRef = document.querySelector('[data-menu]');
   const mobileBtnClose = document.querySelector('[data-menu-close]');
+  const mobileListClose = document.querySelector('[data-anchor-close]');
+  const mobileBuyClose = document.querySelector('[data-buy-close]');
 
   menuBtnRef.addEventListener('click', () => {
     mobileMenuRef.classList.toggle('is-open');
@@ -12,4 +14,15 @@
     mobileMenuRef.classList.toggle('is-open');
     // document.body.classList.toggle('body-hidden');
   });
+
+    mobileListClose.addEventListener('click', () => {
+    mobileMenuRef.classList.toggle('is-open');
+    // document.body.classList.toggle('body-hidden');
+  });
+
+      mobileBuyClose.addEventListener('click', () => {
+    mobileMenuRef.classList.toggle('is-open');
+    // document.body.classList.toggle('body-hidden');
+  });
+
 })();
